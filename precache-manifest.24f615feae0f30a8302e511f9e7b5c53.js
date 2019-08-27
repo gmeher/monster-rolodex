b85e5974cac67f3377f2e56091367674e5820f41
gmeher <gmeher360@gmail.com>
2019-08-27 15:06:05 +0530

    Updates

---
+++ precache-manifest.24f615feae0f30a8302e511f9e7b5c53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be527c9c5fdfc1ff2e68682466185490",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f13749286821263b2cee",
+    "url": "/monster-rolodex/static/css/main.487f9793.chunk.css"
+  },
+  {
+    "revision": "9c72d5c75faf78df56aa",
+    "url": "/monster-rolodex/static/js/2.5a32cafc.chunk.js"
+  },
+  {
+    "revision": "f13749286821263b2cee",
+    "url": "/monster-rolodex/static/js/main.c18b65ba.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  }
+]);
